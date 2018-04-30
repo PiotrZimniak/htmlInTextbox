44b5d40d49e01454a3b5722c0b01821409193608
PiotrZimniak <30718114+PiotrZimniak@users.noreply.github.com>
2018-04-30 16:31:46 +0200

    v0.1

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace htmlInTextbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.valueTextBox = new System.Windows.Forms.TextBox();
+            this.test1Button = new System.Windows.Forms.Button();
+            this.test2Button = new System.Windows.Forms.Button();
+            this.test3Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // valueTextBox
+            // 
+            this.valueTextBox.Location = new System.Drawing.Point(12, 43);
+            this.valueTextBox.Name = "valueTextBox";
+            this.valueTextBox.Size = new System.Drawing.Size(408, 20);
+            this.valueTextBox.TabIndex = 0;
+            // 
+            // test1Button
+            // 
+            this.test1Button.Location = new System.Drawing.Point(12, 12);
+            this.test1Button.Name = "test1Button";
+            this.test1Button.Size = new System.Drawing.Size(132, 25);
+            this.test1Button.TabIndex = 1;
+            this.test1Button.Text = "test1";
+            this.test1Button.UseVisualStyleBackColor = true;
+            this.test1Button.Click += new System.EventHandler(this.test1Button_Click);
+            // 
+            // test2Button
+            // 
+            this.test2Button.Location = new System.Drawing.Point(150, 12);
+            this.test2Button.Name = "test2Button";
+            this.test2Button.Size = new System.Drawing.Size(132, 25);
+            this.test2Button.TabIndex = 2;
+            this.test2Button.Text = "test2";
+            this.test2Button.UseVisualStyleBackColor = true;
+            this.test2Button.Click += new System.EventHandler(this.test2Button_Click);
+            // 
+            // test3Button
+            // 
+            this.test3Button.Location = new System.Drawing.Point(288, 12);
+            this.test3Button.Name = "test3Button";
+            this.test3Button.Size = new System.Drawing.Size(132, 25);
+            this.test3Button.TabIndex = 3;
+            this.test3Button.Text = "test3";
+            this.test3Button.UseVisualStyleBackColor = true;
+            this.test3Button.Click += new System.EventHandler(this.test3Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 82);
+            this.Controls.Add(this.test3Button);
+            this.Controls.Add(this.test2Button);
+            this.Controls.Add(this.test1Button);
+            this.Controls.Add(this.valueTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox valueTextBox;
+        private System.Windows.Forms.Button test1Button;
+        private System.Windows.Forms.Button test2Button;
+        private System.Windows.Forms.Button test3Button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace htmlInTextbox
+{
+    public partial class Form1 : Form
+    {
+        private const string Test1HtmlTag = "<b>{0}</b>";
+
+        private const string Test2HtmlTag = "<p>{0}</p>";
+
+        private const string Test3HtmlTag = "</br>";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReplaceText(string text)
+        {
+            var startIndex = this.valueTextBox.SelectionStart;
+            var selectedTextLength = this.valueTextBox.SelectionLength;
+            var newText = string.Format(text, this.valueTextBox.SelectedText);
+            this.valueTextBox.Text = this.valueTextBox.Text.Remove(startIndex, selectedTextLength);
+            this.valueTextBox.Text = this.valueTextBox.Text.Insert(startIndex, newText);
+            this.valueTextBox.SelectionStart = startIndex + newText.Length;
+        }
+
+        private void InsertText(string text)
+        {
+            var selectionStart = this.valueTextBox.SelectionStart;
+            this.valueTextBox.Text = this.valueTextBox.Text.Insert(this.valueTextBox.SelectionStart, text);
+            this.valueTextBox.SelectionStart = selectionStart + text.Length;
+        }
+
+        private void test1Button_Click(object sender, EventArgs e)
+        {
+            this.ReplaceText(Test1HtmlTag);
+        }
+
+        private void test2Button_Click(object sender, EventArgs e)
+        {
+            this.ReplaceText(Test2HtmlTag);
+        }
+
+        private void test3Button_Click(object sender, EventArgs e)
+        {
+            this.InsertText(Test3HtmlTag);
+        }
+    }
+}
